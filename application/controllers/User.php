67b29716bdb3eebf4f721e6b0b0abea0761c3900
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2021-02-16 15:01:19 +0700

    Adding User Page
    Adding User Profile Page (Controller and View) using SBAdmin 2 Blank Templates,
    Add new default.jpg image profile (default image),
    Seperate template (header, sidebar, topbar, and footer)

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,8 +6,12 @@ class User extends CI_Controller
   public function index()
   {
     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata['email']])->row_array();
-    echo 'Halo';
-    var_dump($data['user']);
-    die;
+    $data['title'] = 'My Profile';
+
+    $this->load->view('user/templates/header', $data);
+    $this->load->view('user/templates/sidebar', $data);
+    $this->load->view('user/templates/topbar', $data);
+    $this->load->view('user/index', $data);
+    $this->load->view('user/templates/footer');
   }
 }
