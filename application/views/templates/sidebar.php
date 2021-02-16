475a91af2ad1efb5f679e7bf179dba90d473e152
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2021-02-16 17:00:50 +0700

    Adding Menu Management System
    Adding user_menu, user_sub_menu, user_access_menu in Database,
    Editing Sidebar templates,
    Adding queryMenu and querySubMenu

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -9,42 +9,52 @@
     <div class="sidebar-brand-text mx-3">DeltaKilo</div>
   </a>
 
-  <!-- Divider -->
-  <hr class="sidebar-divider">
 
+  <?php
+  $role_id = $this->session->userdata('role_id');
+  $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                FROM `user_menu` JOIN `user_access_menu` 
+                ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                WHERE `user_access_menu`.`role_id` = $role_id 
+                ORDER BY `user_access_menu`.`menu_id` ASC
+                ";
+  $menu = $this->db->query($queryMenu)->result_array();
+  ?>
   <!-- Heading -->
-  <div class="sidebar-heading">
-    Administrator
-  </div>
+  <?php foreach ($menu as $m) : ?>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">
+      <?= $m['menu']; ?>
+    </div>
 
-  <!-- Nav Item - Dashboard -->
-  <li class="nav-item">
-    <a class="nav-link" href="index.html">
-      <i class="fas fa-fw fa-tachometer-alt"></i>
-      <span>Dashboard</span></a>
-  </li>
+    <?php
+    $menu_id = $m['id'];
+    $querySubMenu = "SELECT *
+                     FROM `user_sub_menu`
+                     WHERE `menu_id` = $menu_id
+                     AND `is_active` = 1
+                     ";
+    $submenu = $this->db->query($querySubMenu)->result_array();
+    ?>
 
-  <!-- Divider -->
-  <hr class="sidebar-divider">
+    <?php foreach ($submenu as $sm) : ?>
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+          <i class="<?= $sm['icon']; ?>"></i>
+          <span><?= $sm['title']; ?></span></a>
+      </li>
 
-  <!-- Heading -->
-  <div class="sidebar-heading">
-    User Profile
-  </div>
-
-  <!-- Nav Item - Charts -->
-  <li class="nav-item">
-    <a class="nav-link" href="charts.html">
-      <i class="fas fa-fw fa-user"></i>
-      <span>My Profile</span></a>
-  </li>
+    <?php endforeach; ?>
+  <?php endforeach; ?>
 
   <!-- Divider -->
   <hr class="sidebar-divider my-0">
 
   <!-- Nav Item - Charts -->
   <li class="nav-item">
-    <a class="nav-link" href="charts.html">
+    <a class="nav-link" href="<?= base_url('auth/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
       <i class="fas fa-fw fa-sign-out-alt"></i>
       <span>Logout</span></a>
   </li>
