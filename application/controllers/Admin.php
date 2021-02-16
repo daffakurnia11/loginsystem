b2735b33cbc16e5c4baae6d116a1c46f4c03b5b3
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2021-02-16 20:11:59 +0700

    Adding User Access (using roles)
    Adding new helper (login_helper.php) and autoload login helper,
    Adding new blocked page

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,9 +6,7 @@ class Admin extends CI_Controller
   public function __construct()
   {
     parent::__construct();
-    if (!$this->session->userdata('email')) {
-      redirect('auth');
-    }
+    is_logged_in();
   }
 
   public function index()
